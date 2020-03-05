d5dbbe4b6f72367fbef5643906dba2f5609f0bd2
Jake Wagoner <jakewagoneredu@gmail.com>
2020-03-05 11:29:57 -0700

    Item.java

---
+++ src/cmpt355/example/hinky/ast/node/Item.java
@@ -0,0 +1,56 @@
+package cmpt355.example.hinky.ast.node;
+
+import cmpt355.example.hinky.util.Lists;
+
+import java.util.List;
+
+public class Item extends AstNode {
+	private String str;
+	private Expression expr;
+
+	public Item(Expression expr) {
+		setExpr(expr);
+	}
+
+	public Item(String str) {
+		setStr(str);
+	}
+
+	public String getStr() {
+		return str;
+	}
+
+	public void setStr(String str) {
+		this.str = str;
+	}
+
+	public Expression getExpr() {
+		return expr;
+	}
+
+	public void setExpr(Expression expr) {
+		this.expr = reparentNonNull(expr);
+	}
+
+	// check type: String
+	public boolean isString() {
+		return (str != undefined);
+	}
+
+	// check type: Expression
+	public boolean isExpression() {
+		return (expr != Null);
+	}
+
+	@Override
+	public List<? extends AstNode> children() {
+		return Lists.<AstNode>builder()     // list builder in util by Kathy
+				.addIfPresent(expr)
+				.build();
+	}
+
+	@Override
+	public String toString() {
+		return "item";
+	}
+}
